7d0abf19dad8d6e7996d48e9582f546a2892d20c
Eduardo Garcia <111912573+eduardo-thedev@users.noreply.github.com>
2023-05-21 00:12:18 -0300

    Create painel.php

---
+++ painel.php
@@ -0,0 +1,38 @@
+<?php
+
+    include('protect.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>N.A.A.T.A.</title>
+    <link rel="shortcut icon" href="trio.png" type="image/x-icon">
+    
+    <link rel="stylesheet" href="painel.css">
+
+
+</head>
+<body>
+    <nav>
+    <div class="logo">
+        <a href="painel.php"> Painel</a>
+    </div>
+
+     <ul>
+        <li> <a href="sobre.html"> Sobre nós </a> </li>
+        <li> <a href="contato.html"> Contato </a> </li>
+        <li> <a href="tratamentos.html"> Tratamentos </a> </li>
+        <li> <a href="depoimentos.html"> Depoimentos </a> </li>
+        <li> <a href="logout.php"> Sair </a> </li>
+    </ul>
+    </nav>
+
+
+</body>
+</html>
